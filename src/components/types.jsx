8bbfc589ac4401fcc770d9c8f08c744903b5328b
AlexLeiba <Alexleiba13@gmail.com>
2023-07-02 17:14:05 +0300

    changes

--- src/components/types.jsx
+++ src/components/types.jsx
@@ -1,7 +1,7 @@
-const WinnerType = {
-  DRAW: "Draw",
-  PLAYER_WON: "You won",
-  DEALER_WON: "Dealer won",
-  YOU_WON_ALL_MONEY: "You won all dealer`s money Congrats!",
-  YOU_LOST_ALL_MONEY: "You lost all your money",
-};
+// const WinnerType = {
+//   DRAW: "Draw",
+//   PLAYER_WON: "You won",
+//   DEALER_WON: "Dealer won",
+//   YOU_WON_ALL_MONEY: "You won all dealer`s money Congrats!",
+//   YOU_LOST_ALL_MONEY: "You lost all your money",
+// };
